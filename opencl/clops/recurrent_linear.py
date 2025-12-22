f338e3d1b83691274d6e132005355ca7bcc03a4a
Zhang Yi <yi3.zhang@intel.com>
2025-12-22 16:35:44 +0800

    save work with ref

--- opencl/clops/recurrent_linear.py
+++ opencl/clops/recurrent_linear.py
@@ -19,20 +19,21 @@ void add(float *a, float* b, size_t n) {
     }
 }
 
-__kernel void recurrent_gated_delta_rule(__global half * q, 
-    __global half * k, 
-    __global half * v, 
-    __global half * g,
-    __global half * beta,
-    __global half * initial_state) {
+__kernel void recurrent_gated_delta_rule(__global float * q, 
+    __global float * k, 
+    __global float * v, 
+    __global float * g,
+    __global float * beta,
+    __global float * initial_state,
+    __global float * output) {
     int b = get_global_id(0);
     int h = get_global_id(1);
     int i_v = get_global_id(2);
     int BATCH_STRIDE = BATCH_NUM * K_HEAD_NUMS * SEQ_LEN;
     int HEAD_STRIDE = SEQ_LEN * K_HEAD_DIMS;
-    half* q_ptr = q + b * BATCH_STRIDE + h * HEAD_STRIDE;
-    half* k_ptr = k + b * BATCH_STRIDE + h * HEAD_STRIDE;
-    half* v_ptr = v + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float* q_ptr = q + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float* k_ptr = k + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float* v_ptr = v + b * BATCH_STRIDE + h * HEAD_STRIDE;
     float init_state[K_HEAD_DIMS] = {0};
     float b_k[K_HEAD_DIMS] = {0};
     float b_q[K_HEAD_DIMS] = {0};
@@ -43,7 +44,6 @@ __kernel void recurrent_gated_delta_rule(__global half * q,
         float b_g = g[b * K_HEAD_NUMS * SEQ_LEN + h * SEQ_LEN + i];
         float b_beta = beta[b * K_HEAD_NUMS * SEQ_LEN + h * SEQ_LEN + i];
         b_g = exp(b_g);
-        printf("b_g %f\n", b_g);
         for (int j = 0; j < K_HEAD_DIMS; j++) {
             b_k[j] = k_ptr[i * K_HEAD_DIMS  + j];
             b_q[j] = q_ptr[i * K_HEAD_DIMS  + j];
@@ -58,7 +58,156 @@ __kernel void recurrent_gated_delta_rule(__global half * q,
         scale(b_k, b_v, K_HEAD_DIMS);
         // h = h0 + update
         add(init_state, b_k, K_HEAD_DIMS);
-        float output  = dot_product(init_state, b_q, K_HEAD_DIMS);
+        float b_output  = dot_product(init_state, b_q, K_HEAD_DIMS);
+        output[b * K_HEAD_NUMS * SEQ_LEN * K_HEAD_DIMS + h * SEQ_LEN * K_HEAD_DIMS + i * K_HEAD_DIMS + i_v] = b_output;
+        //printf("b %d h %d i_v %d seq %i output %f init_state %f\n", b, h, i_v, i, b_output, init_state[0]);
+    }
+
+/*
+    printf("b %d h %d get_sub_group_local_id %d\n", b, h, get_sub_group_local_id());
+    for (size_t i = 0; i < T; i++) {
+        float partial = 0.0f;
+        
+        for (int j = lid; j < 16; j += lsize) {
+            half a = as_half(intel_sub_group_block_read_us((__global ushort*)(q_ptr + j + i *16)));
+            partial += a;
+        }
+        float sub_sum = sub_group_reduce_add(partial);
+
+        if (get_sub_group_local_id() == 0) {
+            printf("batch %d head %d q %zu value %f\n", b, h, i, sub_sum);
+        }
+    }
+*/
+}
+
+'''
+
+cm_kernel_sources = r'''
+float dot_product(float* a, float* b, size_t n) {
+    float result = 0.0f;
+    for (size_t i = 0; i < n; i++) {
+        result += a[i] * b[i];
+    }
+    return result;
+}
+
+void scale(float *a, float scale, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        a[i] = a[i] * scale;
+    }
+}
+
+void add(float *a, float* b, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        a[i] += b[i];
+    }
+}
+
+__kernel void recurrent_gated_delta_rule_v0(__global float * q, 
+    __global float * k, 
+    __global float * v, 
+    __global float * g,
+    __global float * beta,
+    __global float * initial_state,
+    __global float * output) {
+    int b = get_global_id(0);
+    int h = get_global_id(1);
+    int i_v = get_global_id(2);
+    int BATCH_STRIDE = BATCH_NUM * K_HEAD_NUMS * SEQ_LEN;
+    int HEAD_STRIDE = SEQ_LEN * K_HEAD_DIMS;
+    float* q_ptr = q + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float* k_ptr = k + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float* v_ptr = v + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float init_state[K_HEAD_DIMS] = {0};
+    float b_k[K_HEAD_DIMS] = {0};
+    float b_q[K_HEAD_DIMS] = {0};
+    for (int j = 0; j < K_HEAD_DIMS; j++) {
+        init_state[j] = initial_state[b * K_HEAD_NUMS * K_HEAD_DIMS * K_HEAD_DIMS + h * K_HEAD_DIMS * K_HEAD_DIMS + j * K_HEAD_DIMS + i_v];
+    }
+    for (int i = 0; i < SEQ_LEN; i++) {
+        float b_g = g[b * K_HEAD_NUMS * SEQ_LEN + h * SEQ_LEN + i];
+        float b_beta = beta[b * K_HEAD_NUMS * SEQ_LEN + h * SEQ_LEN + i];
+        b_g = exp(b_g);
+        for (int j = 0; j < K_HEAD_DIMS; j++) {
+            b_k[j] = k_ptr[i * K_HEAD_DIMS  + j];
+            b_q[j] = q_ptr[i * K_HEAD_DIMS  + j];
+        }
+        // h0 * g
+        scale(init_state, b_g, K_HEAD_DIMS);
+        float h_k = dot_product(init_state, b_k, K_HEAD_DIMS);
+        float b_v = v_ptr[i_v + i * K_HEAD_DIMS];
+        b_v -= h_k;
+        // b_v * b_k
+        b_v *= b_beta;
+        scale(b_k, b_v, K_HEAD_DIMS);
+        // h = h0 + update
+        add(init_state, b_k, K_HEAD_DIMS);
+        float b_output  = dot_product(init_state, b_q, K_HEAD_DIMS);
+        output[b * K_HEAD_NUMS * SEQ_LEN * K_HEAD_DIMS + h * SEQ_LEN * K_HEAD_DIMS + i * K_HEAD_DIMS + i_v] = b_output;
+        //printf("b %d h %d i_v %d seq %i output %f init_state %f\n", b, h, i_v, i, b_output, init_state[0]);
+    }
+
+/*
+    printf("b %d h %d get_sub_group_local_id %d\n", b, h, get_sub_group_local_id());
+    for (size_t i = 0; i < T; i++) {
+        float partial = 0.0f;
+        
+        for (int j = lid; j < 16; j += lsize) {
+            half a = as_half(intel_sub_group_block_read_us((__global ushort*)(q_ptr + j + i *16)));
+            partial += a;
+        }
+        float sub_sum = sub_group_reduce_add(partial);
+
+        if (get_sub_group_local_id() == 0) {
+            printf("batch %d head %d q %zu value %f\n", b, h, i, sub_sum);
+        }
+    }
+*/
+}
+
+__kernel void recurrent_gated_delta_rule(__global float * q, 
+    __global float * k, 
+    __global float * v, 
+    __global float * g,
+    __global float * beta,
+    __global float * initial_state,
+    __global float * output) {
+    int b = get_global_id(0);
+    int h = get_global_id(1);
+    int i_v = get_global_id(2);
+    int BATCH_STRIDE = BATCH_NUM * K_HEAD_NUMS * SEQ_LEN;
+    int HEAD_STRIDE = SEQ_LEN * K_HEAD_DIMS;
+    float* q_ptr = q + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float* k_ptr = k + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float* v_ptr = v + b * BATCH_STRIDE + h * HEAD_STRIDE;
+    float init_state[K_HEAD_DIMS] = {0};
+    float b_k[K_HEAD_DIMS] = {0};
+    float b_q[K_HEAD_DIMS] = {0};
+    for (int j = 0; j < K_HEAD_DIMS; j++) {
+        init_state[j] = initial_state[b * K_HEAD_NUMS * K_HEAD_DIMS * K_HEAD_DIMS + h * K_HEAD_DIMS * K_HEAD_DIMS + j * K_HEAD_DIMS + i_v];
+    }
+    for (int i = 0; i < SEQ_LEN; i++) {
+        float b_g = g[b * K_HEAD_NUMS * SEQ_LEN + h * SEQ_LEN + i];
+        float b_beta = beta[b * K_HEAD_NUMS * SEQ_LEN + h * SEQ_LEN + i];
+        b_g = exp(b_g);
+        for (int j = 0; j < K_HEAD_DIMS; j++) {
+            b_k[j] = k_ptr[i * K_HEAD_DIMS  + j];
+            b_q[j] = q_ptr[i * K_HEAD_DIMS  + j];
+        }
+        // h0 * g
+        scale(init_state, b_g, K_HEAD_DIMS);
+        float h_k = dot_product(init_state, b_k, K_HEAD_DIMS);
+        float b_v = v_ptr[i_v + i * K_HEAD_DIMS];
+        b_v -= h_k;
+        // b_v * b_k
+        b_v *= b_beta;
+        scale(b_k, b_v, K_HEAD_DIMS);
+        // h = h0 + update
+        add(init_state, b_k, K_HEAD_DIMS);
+        float b_output  = dot_product(init_state, b_q, K_HEAD_DIMS);
+        output[b * K_HEAD_NUMS * SEQ_LEN * K_HEAD_DIMS + h * SEQ_LEN * K_HEAD_DIMS + i * K_HEAD_DIMS + i_v] = b_output;
+        //printf("b %d h %d i_v %d seq %i output %f init_state %f\n", b, h, i_v, i, b_output, init_state[0]);
     }
 
 /*
@@ -84,6 +233,7 @@ __kernel void recurrent_gated_delta_rule(__global half * q,
 from . import cl
 import numpy as np
 from .utils import *
+import torch.nn.functional as F
 
 def recurrent_gated_delta_rule_ref(
     q: torch.Tensor,
@@ -134,16 +284,17 @@ class RecurrentGDN:
         self.V = V
         self.kernels = kernel_cache(cl_kernel_sources, f"-DBATCH_NUM={self.B} -DK_HEAD_NUMS={self.H} -DSEQ_LEN={self.T} -DK_HEAD_DIMS={self.K}", "./dump/")
     
-    def __call__(self, q, k, v, g, beta, hidden_states):
+    def __call__(self, q, k, v, g, beta, hidden_states, o):
         self.kernels.enqueue("recurrent_gated_delta_rule",
-                            [self.B, self.H, self.V],
-                            [1, 1, 1],
+                            [self.B, self.H, self.V * 16],
+                            [1, 1, 16],
                             q,
                             k,
                             v,
                             g,
                             beta,
-                            hidden_states
+                            hidden_states,
+                            o
                             )
 
 if __name__ == "__main__":
@@ -156,12 +307,24 @@ if __name__ == "__main__":
     K = 16
     V = 16
     torch.set_printoptions(sci_mode=False)
-    q = torch.zeros(B*H*T*K, dtype=torch.float16).reshape([B, H, T, K])
-    k = torch.zeros(B*H*T*K, dtype=torch.float16).reshape([B, H, T, K])
-    v = torch.zeros(B*H*T*V, dtype=torch.float16).reshape([B, H, T, V])
-    beta = torch.zeros(B*H*T, dtype=torch.float16).reshape([B, H, T])
-    g = torch.zeros(B*H*T, dtype=torch.float16).reshape([B, H, T])
-    hidden_states = torch.randn(B, H, K, V, dtype=torch.float16)
+    dtype = torch.float32
+    # q = torch.randn(B*H*T*K, dtype=dtype).reshape([B, H, T, K])
+    # k = torch.randn(B*H*T*K, dtype=dtype).reshape([B, H, T, K])
+    # v = torch.randn(B*H*T*V, dtype=dtype).reshape([B, H, T, V])
+    # beta = torch.rand(B, H, T, dtype=dtype).sigmoid()
+    # g = F.logsigmoid(torch.rand(B, H, T, dtype=dtype))
+    # o = torch.randn(B, H, T, V, dtype=dtype).to(v)
+    # hidden_states = torch.randn(B, H, K, V, dtype=dtype)
+
+    q = torch.zeros(B*H*T*K, dtype=dtype).reshape([B, H, T, K])
+    k = torch.zeros(B*H*T*K, dtype=dtype).reshape([B, H, T, K])
+    v = torch.zeros(B*H*T*V, dtype=dtype).reshape([B, H, T, V])
+    beta = torch.zeros(B*H*T, dtype=dtype).reshape([B, H, T])
+    g = torch.zeros(B*H*T, dtype=dtype).reshape([B, H, T])
+    o = torch.zeros(B, H, T, V).to(v)
+    q = F.normalize(q, p=2, dim=-1)
+    k = F.normalize(k, p=2, dim=-1)
+    hidden_states = torch.randn(B, H, K, V, dtype=dtype)
     for h in range(H):
         for t in range(T):
             q[0, h, t, :] = 0 + h + 1
@@ -169,7 +332,7 @@ if __name__ == "__main__":
             v[0, h, t, :] = 0 + h + 3
             beta[0, h, t] = 1
             g[0, h, t] = 0
-            # hidden_states[0, h, :, :] = torch.ones([K, V])
+            hidden_states[0, h, :, :] = torch.ones([K, V])
 
 
     print("q ", q)
@@ -181,11 +344,19 @@ if __name__ == "__main__":
     cl_g = to_cl(g)
     cl_beta = to_cl(beta)
     cl_hidden_states = to_cl(hidden_states)
+    cl_o = to_cl(o)
     recurrnGDN = RecurrentGDN(B, H, T, K, V)
-    recurrnGDN(cl_q, cl_k, cl_v, cl_g, cl_beta, cl_hidden_states)
-    o, h = recurrent_gated_delta_rule_ref(q, k, v, beta, g, 1.0, hidden_states, output_final_state=True)
-    print("o ", o)
-    print("h ", h)
+    n_times = 1
+    print(n_times)
+    recurrnGDN(cl_q, cl_k, cl_v, cl_g, cl_beta, cl_hidden_states, cl_o)
+    # durs = cl.finish()
+    # Bsize = (q.numel() + k.numel() + v.numel() + g.numel() + beta.numel() + o.numel()) * 4
+    # for ns in durs:
+    #     print(f"{Bsize*1e-6:.3f} MB {ns*1e-6:.3f} ms, BW: { Bsize/ns : .2f} GB/s")
+    o_ref, h_ref = recurrent_gated_delta_rule_ref(q, k, v, beta, g, 1.0, hidden_states, output_final_state=True)
+    print("o ", to_torch(cl_o))
+    print("o_fef", o_ref)
+    # print("h ", h)
     # print(to_torch(cl_q))
     # for i in range(10):
     #     vecAdd(input_a, input_b, output)
